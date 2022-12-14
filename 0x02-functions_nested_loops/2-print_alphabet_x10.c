11dd669bf1b94a79aead00147551b872e081742d
Vicki50 <victoriajose2008@yahoo.com>
2022-12-14 06:41:31 -0800

    task4

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  */
 
 void print_alphabet_x10(void)
-{
+{	
 	int count = 0;
 	char letter;
 
@@ -14,5 +14,4 @@ void print_alphabet_x10(void)
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
-	}
-
+	}	
