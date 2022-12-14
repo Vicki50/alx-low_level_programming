26d3f9138729589c30a35f7a49d1df5f71b40ced
Vicki50 <victoriajose2008@yahoo.com>
2022-12-14 10:03:57 -0800

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * print_last_digit - prints the last digit of a number
  * @n: The number to be treated
  *
- *Return: Value of the last digit of number
+ * Return: Value of the last digit of number
  */
 int print_last_digit(int n)
 {
