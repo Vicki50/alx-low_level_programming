b3650ffb7db9bfa153f94bfddebd9f1663b113c6
Vicki50 <victoriajose2008@yahoo.com>
2022-12-16 08:56:51 -0800

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,12 @@
  *return: 0
  */
 
-int main (void)
+int main(void)
 {
-    int i;
-    i = 0;
-    positive_or_negative(i);
+	int i;
 
-    return(0);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
